2e32e14dbf5a7a45c8f8b60ba4c7b563147f57dd
Анатолий Орлов <FluxGen@dr.com>
2021-05-29 19:45:49 +0300

    школа 1 готово

--- PP/main.Designer.cs
+++ PP/main.Designer.cs
@@ -33,33 +33,42 @@ namespace PP
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(434, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Form1";
+            this.button1.Text = "Form1 моё";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(0, 23);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(434, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Form2";
+            this.button2.Text = "Form2 для Леры";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 70);
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.Location = new System.Drawing.Point(0, 46);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(434, 23);
             this.button3.TabIndex = 2;
             this.button3.Text = "auth";
             this.button3.UseVisualStyleBackColor = true;
@@ -67,24 +76,99 @@ namespace PP
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(12, 99);
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.Location = new System.Drawing.Point(0, 69);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(434, 23);
             this.button4.TabIndex = 3;
             this.button4.Text = "аптека";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.Location = new System.Drawing.Point(0, 92);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(434, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "школа 1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button6.Location = new System.Drawing.Point(0, 115);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(434, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "школа 2";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button7.Location = new System.Drawing.Point(0, 138);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(434, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "мясокомбинат";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button8.Location = new System.Drawing.Point(0, 161);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(434, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "неверный логин или пароль";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button9.Location = new System.Drawing.Point(0, 184);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(434, 23);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Неверный ввод данных";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button10.Location = new System.Drawing.Point(0, 207);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(434, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "непредвиденная ошибка";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
             // main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(434, 231);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "main";
             this.ResumeLayout(false);
 
@@ -96,5 +180,11 @@ namespace PP
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
     }
 }
--- PP/main.cs
+++ PP/main.cs
@@ -40,5 +40,38 @@ namespace PP
             аптека form = new аптека();
             form.Show();
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            школа_1 form = new школа_1();
+            form.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            школа_2 form = new школа_2();
+            form.Show();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            мясокомбинат form = new мясокомбинат();
+            form.Show();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Неверный логин или пароль", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Неверный ввод данных", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Произошла непредвиденная ошибка", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
     }
 }
